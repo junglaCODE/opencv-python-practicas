6cf8836e81081b6b3e2502ce8624eab523fdd681
JLGC/@monolinux <jlgarcia@junglacode.org>
2019-12-03 10:53:15 -0600

    Base para captura del streaming

---
+++ __init__.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+Ip_Cam =  { 
+        'left'    :   '192.168.100.20', 
+        'middle'  :   '192.168.100.17', 
+        'right'   :   '192.168.100.19'
+     }
+
+
+Streaming = cv2.VideoCapture('rtsp://'+Ip_Cam['middle']+'/stream1')
+
+while True:
+  _ret,_frames= Streaming.read()
+
+  if _ret==True:
+    cv2.imshow('Video de la camara', _frame)
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+      break
+
+Streaming.release()
