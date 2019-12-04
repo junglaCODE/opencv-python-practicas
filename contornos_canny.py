6f095d0ee5c8d4568bbf9a2b89ee948b8d1e0f38
JLGC/@monolinux <jlgarcia@junglacode.org>
2019-12-04 12:30:20 -0600

    añadiendo el algoritmo canny

---
+++ contornos_canny.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+def saveAbstraccionObjects(name , data) : 
+    _data = ' '.join([str(elem) for elem in data]) 
+    archivo = open('abstracciones/'+name+'.dat', 'w+')
+    archivo.write(_data)
+    archivo.close() 
+
+_img = 'silla'
+_ext = '.jpg'
+_resource = 'assets/'+_img+_ext
+
+img = cv2.imread(_resource)
+render = cv2.cvtColor(cv2.imread(_resource),cv2.COLOR_BGR2GRAY)
+canny = cv2.Canny(render,50,500)
+canny = cv2.dilate(canny, None, iterations=1)
+canny = cv2.erode(canny, None, iterations=1)
+
+contours , hirarchy = cv2.findContours(canny,
+        cv2.RETR_TREE , 
+        cv2.CHAIN_APPROX_SIMPLE
+    )
+
+saveAbstraccionObjects(_img+'_h',hirarchy)
+saveAbstraccionObjects(_img+'_c',contours)
+
+cv2.drawContours(img,contours, -1 , (123,11,11) , 2)
+cv2.imshow('canny', np.hstack([render , canny]))
+cv2.imshow('buscador contornos', img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
