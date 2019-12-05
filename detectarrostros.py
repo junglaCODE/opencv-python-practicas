e7642931011b1a365eee7846dfd97b0d6a1843e9
= <=>
2019-12-05 07:16:27 -0600

    reconociendo rostros en video

--- detectarrostros.py
+++ detectarrostros.py
@@ -4,7 +4,7 @@ import numpy as np
 _red_neuronal = 'redes_neuronales/haarcascade_frontalface_default.xml'
 faceClassif = cv2.CascadeClassifier(_red_neuronal)
 cv2.namedWindow("Detectar Rostros", cv2.WINDOW_NORMAL)  
-
+'''
 #imagen estatica
 image = cv2.imread('assets/oficina.png')
 
@@ -39,15 +39,14 @@ while True:
 		gray ,
 		scaleFactor=1.1,
 		minNeighbors=11,
-		minSize=(100,100),
+		minSize=(50,50),
 		maxSize=(400,400)
 	)
 
 	for (x,y,w,h) in faces:
 		cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
 
-	cv2.imshow('Detectar Rostros',gray)
+	cv2.imshow('Detectar Rostros',frame)
 	
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
-'''
